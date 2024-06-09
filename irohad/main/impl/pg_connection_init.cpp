9cb6b50a7a1e5da30fe429020345928cc325b672
Haruka <haruka-h@ogl.is.ocha.ac.jp>
2024-06-09 19:00:05 +0900

    offchainDBの追加

--- irohad/main/impl/pg_connection_init.cpp
+++ irohad/main/impl/pg_connection_init.cpp
@@ -209,8 +209,8 @@ namespace {
     try {
       return getMaintenanceSession(postgres_options) |
           [&](auto maintenance_sql) {
-            *maintenance_sql << fmt::format("create database %s; create database %s;")
-                                            %postgres_options.workingDbName()%postgres_options.workingDbName_offDB();
+            *maintenance_sql << fmt::format("create database {}; create database offchainDB;",
+                                            postgres_options.workingDbName);
             return getWorkingDbSession(postgres_options) | [](auto session)
                        -> iroha::expected::Result<void, std::string> {
               prepareTables(*session);
