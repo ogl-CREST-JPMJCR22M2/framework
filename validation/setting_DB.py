ba8ab5e942777e3603f3749912ae8c98eb197fb5
Haruka <haruka-h@ogl.is.ocha.ac.jp>
2024-06-10 10:49:52 +0900

    CREATE DB

---
+++ validation/setting_DB.py
@@ -0,0 +1,33 @@
+from typing import Optional
+from psycopg2 import connect
+from psycopg2._psycopg import connection, cursor
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+dsn = {
+    "dbname": "postgres",
+    "user": "postgres",
+    "password": "postgres",
+    "port": "5432",
+    "host": "localhost",
+}
+
+conn: Optional[connection] = None
+try:
+    conn = connect(**dsn)
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+    with conn.cursor() as cur:
+        cur: cursor
+
+        cur.execute(
+            "CREATE DATABASE offchainDB;
+            CREATE TABLE IF NOT EXISTS offchainDB_CO2EMISSIONS(
+                    PartsID CHARACTER varying(288),
+                    TotalEMISSIONS DECIMAL NOT NULL ,
+                    EMISSIONS DECIMAL NOT NULL ,
+                    PRIMARY KEY (PartsID)
+         );"
+        )
+finally:
+    if conn:
+        conn.close()
