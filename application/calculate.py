8f553db344a60bc9fe1774aca3d2e131dccc112f
Haruka <haruka-h@ogl.is.ocha.ac.jp>
2025-03-26 04:03:07 +0000

    3/26

--- application/calculate.py
+++ application/calculate.py
@@ -52,15 +52,78 @@ def get_childpart(df, parents_partid):
 
     return child_df
     
+# ハッシュ化    
+def hash_data(self, data):
+        return hashlib.sha256(data.encode()).hexdigest()
 
 def get_hash(df, target_id):
 
-    df = df.with_row_index()
-    df = 
+    # 動作確認の値セット  
+    df_h = pl.DataFrame(
+        {
+            "partid" : ["P"+str(i) for i in range(1, 156)],
+            "part_hash" : ["H"+str(i) for i in range(1, 156)],
+            #"part_hash" : ["h"+str(i) for i in range(1, 156)],
+        }
+    )
+
+    df = df.join(df_h, on=["partid"], how="left")
+
+    #葉ノードの抽出
+    parent_counts = df["parents_partid"].value_counts().rename({"parents_partid": "partid"})
+    df = df.join(parent_counts, on=["partid"], how="left").fill_null(0)
+
+
+    #ハッシュの算出
+    #df = df.with_columns(
+    #    pl.col("partid").hash().alias("cfpval_hash")
+    #)  
+
+    #葉ノードには hash ← part_hash
+    
+    df = df.with_columns(
+        pl.when(pl.col("count") != 0)
+        .then(pl.lit(float("NAN")))  # NaN を代入
+        .otherwise(pl.col("part_hash"))  # それ以外は元の値を保持
+        .alias("hash")  # カラム名を指定
+    ) 
+
+    #葉ノードをキューに追加
+
+    sorted_parts = []
+    processing_queue = df.filter(pl.col("count") == 0)["partid"].to_list()
+
+    while processing_queue:
+        
+        part = processing_queue.pop(0)  # 先頭を処理
+
+        parenttpart = df.filter(pl.col("partid") == part)["parents_partid"].to_list() # 先頭partの親部品の取得
+
+        if not parenttpart : break # ルート部品ならbreak
+
+        df.filter(pl.col("partid") == part)["part_hash"].item()
+
+        #hashed_part = parenttpart[0] + part
+
+        c = parent_counts.filter(pl.col("partid") == parenttpart[0])["count"].item() # 子部品数の取得
+
+        
+        
+        for i in range(c-1):
+            part = part + processing_queue.pop(0)
+            
+        part = "(" + part + ")"
+
+        print(part)
+
+        #親をキューに追加
+        processing_queue.extend(parenttpart)
+
+
+    return df
 
 
 
-    return hashed
 
 if __name__ == '__main__':
 
